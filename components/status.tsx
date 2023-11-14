cd8dce339dbfe4390f9601e79ec37e1db698c315
ablydevin <136481955+ablydevin@users.noreply.github.com>
2023-11-14 14:48:28 -0500

    Added client ID display

--- components/status.tsx
+++ components/status.tsx
@@ -2,16 +2,20 @@
 
 import Image from 'next/image'
 import { useState } from 'react'
-import { useConnectionStateListener } from 'ably/react'
+import { useConnectionStateListener, useAbly } from 'ably/react'
 
 export default function Status() {
 
+  const client = useAbly();
     const [state, setState] = useState('')
     useConnectionStateListener((stateChange) => {
       setState(stateChange.current)
     });
   
     return (
+      <div>
         <p><span>Realtime Connection Status: {state}</span></p>
+        <p><span>ClientId: {client.auth.clientId}</span></p>
+      </div>
     )
 }
