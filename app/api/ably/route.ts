dac9a25acb7a406686ff98b9e880b5bab9cbecba
bookercodes <booker@booker.codes>
2023-11-21 14:35:38 +0100

    Remove authentication comment

--- app/api/ably/route.ts
+++ app/api/ably/route.ts
@@ -3,10 +3,6 @@ import { currentUser } from "@clerk/nextjs";
 
 export const GET = async (req:Request, res:Response) => {
   const user = await currentUser();
- // Your application should perform some type of user 
- // authorization to validate that the user is allowed 
- // to receive a token before fulfilling the token request 
- // if (requesting_user.isAuthenticated) {
 
     const clientId = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
 
@@ -17,7 +13,4 @@ export const GET = async (req:Request, res:Response) => {
     console.log(`Request: ${JSON.stringify(tokenRequestData)}`);
     return Response.json(tokenRequestData);
 
-  //} else {
- // res.status(401).json({ 'error':'User is not authorized' })
-  //}
 };
