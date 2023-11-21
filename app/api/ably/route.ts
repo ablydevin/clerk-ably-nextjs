fb5090313dbcd50a855ffe0d2af77a385bf20211
bookercodes <booker@booker.codes>
2023-11-21 14:36:02 +0100

    Format route.ts indentation

--- app/api/ably/route.ts
+++ app/api/ably/route.ts
@@ -1,16 +1,16 @@
 import Ably from "ably/promises";
 import { currentUser } from "@clerk/nextjs";
 
-export const GET = async (req:Request, res:Response) => {
+export const GET = async (req: Request, res: Response) => {
   const user = await currentUser();
 
-    const clientId = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+  const clientId = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
 
-    const client = new Ably.Rest(process.env.NEXT_ABLY_API_KEY as string);
-    const tokenRequestData = await client.auth.createTokenRequest({
-      clientId: user?.username as string 
-    });
-    console.log(`Request: ${JSON.stringify(tokenRequestData)}`);
-    return Response.json(tokenRequestData);
+  const client = new Ably.Rest(process.env.NEXT_ABLY_API_KEY as string);
+  const tokenRequestData = await client.auth.createTokenRequest({
+    clientId: user?.username as string
+  });
+  console.log(`Request: ${JSON.stringify(tokenRequestData)}`);
+  return Response.json(tokenRequestData);
 
 };
